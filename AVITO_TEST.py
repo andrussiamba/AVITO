ae9bacea03a156f27cb5c4c7322825b535fe4e1c
andrussiamba <158198810+andrussiamba@users.noreply.github.com>
2024-04-14 22:27:34 +0300

    Update AVITO_TEST.py

--- AVITO_TEST.py
+++ AVITO_TEST.py
@@ -4,16 +4,22 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.avito.ru/avito-care/eco-impact")
 url=driver.current_url
-time.sleep(5)
+driver.fullscreen_window()
+time.sleep(3)
 capture_path = 'D:/OUTPUT/1.png'
 driver.save_screenshot(capture_path)
-time.sleep(2)
+time.sleep(1)
 driver.find_element("name", "arrow").click()
-time.sleep(2)
+time.sleep(1)
 capture_path = 'D:/OUTPUT/2.png'
 driver.save_screenshot(capture_path)
-time.sleep(2)
+time.sleep(1)
 driver.find_element("name", "arrow").click()
-time.sleep(2)
+time.sleep(1)
 capture_path = 'D:/OUTPUT/3.png'
 driver.save_screenshot(capture_path)
+time.sleep(1)
+driver.execute_script("window.scrollBy(0,850)","")
+capture_path = 'D:/OUTPUT/4.png'
+driver.save_screenshot(capture_path)
+time.sleep(1)
